6f60aff1bc61ee9ee61bc43d502c46e1b44de17a
ja.avelino@uniandes.edu.co <>
2019-08-21 08:59:34 -0500

    Se creo la entidad para Mascota Encontrada

---
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/entities/MascotaEncontradaEntity.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author ja.avelino
+ */
+@Entity
+public class MascotaEncontradaEntity extends BaseEntity implements Serializable{
+    
+    /**
+     * Variable que modela la especie de la mascota
+     */
+    private String especie;
+    
+    /**
+     * Variable que modela la raza de la mascota
+     */
+    private String raza;
+    
+    /**
+     * Variable que modela el lugar donde se encontro la mascota
+     */
+    private String lugar;
+    
+    /**
+     * Variable que modela la descripcion de la mascota
+     */
+    private String descripcion;
+    
+    /**
+     * Variable que modela la fecha cuando se encontro la mascota
+     */
+    @Temporal(TemporalType.DATE)
+    private Date fechaEncontrada;
+
+    /**
+     * @return the especie
+     */
+    public String getEspecie() {
+        return especie;
+    }
+
+    /**
+     * @param especie the especie to set
+     */
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    /**
+     * @return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return the lugar
+     */
+    public String getLugar() {
+        return lugar;
+    }
+
+    /**
+     * @param lugar the lugar to set
+     */
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fechaEncontrada
+     */
+    public Date getFechaEncontrada() {
+        return fechaEncontrada;
+    }
+
+    /**
+     * @param fechaEncontrada the fechaEncontrada to set
+     */
+    public void setFechaEncontrada(Date fechaEncontrada) {
+        this.fechaEncontrada = fechaEncontrada;
+    }
+    
+}
