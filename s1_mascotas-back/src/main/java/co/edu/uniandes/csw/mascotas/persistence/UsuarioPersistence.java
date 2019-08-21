7f782b71fa5c0979558858904791e632a6446c73
Tobia Gasparoni <tr.gasparoni@uniandes.edu.co>
2019-08-21 13:14:13 -0500

    Clase Usuario creada

---
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/UsuarioPersistence.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.persistence;
+
+import co.edu.uniandes.csw.mascotas.entities.UsuarioEntity;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Estudiante
+ */
+@Stateless
+public class UsuarioPersistence
+{
+    public UsuarioEntity create(UsuarioEntity usuario)
+    {
+        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+    }
+}
