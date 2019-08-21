a5b2a54de2f8b10abdd9dd8136eedc0eeee16bb1
LilyAita <la.duque@uniandes.edu.co>
2019-08-21 09:07:28 -0500

    Crear tabla MascotaPerdida

---
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/entities/MascotaPerdidaEntity.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Lily Duque
+ */
+@Entity
+public class MascotaPerdidaEntity extends BaseEntity implements Serializable
+{
+    
+    private String raza;
+    private String especie;
+    private String descripcion;
+    private String lugar;
+    @Temporal(TemporalType.DATE)
+    private Date fechaPerdida;
+
+    /**
+     * @return the raza
+     */
+    public String getRaza() {
+        return raza;
+    }
+
+    /**
+     * @param raza the raza to set
+     */
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    /**
+     * @return the especie
+     */
+    public String getEspecie() {
+        return especie;
+    }
+
+    /**
+     * @param especie the especie to set
+     */
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    /**
+     * @return the descripcion
+     */
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    /**
+     * @return the fechaPerdida
+     */
+    public Date getFechaPerdida() {
+        return fechaPerdida;
+    }
+
+    /**
+     * @param fechaPerdida the fechaPerdida to set
+     */
+    public void setFechaPerdida(Date fechaPerdida) {
+        this.fechaPerdida = fechaPerdida;
+    }
+
+    /**
+     * @return the lugar
+     */
+    public String getLugar() {
+        return lugar;
+    }
+
+    /**
+     * @param lugar the lugar to set
+     */
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+    
+}
