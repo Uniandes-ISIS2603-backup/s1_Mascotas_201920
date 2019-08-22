0670f12bb0e15598b6e24801743f5a763d6d1f3c
William Smith <wr.smith10@uniandes.edu.co>
2019-08-22 09:21:42 -0500

    Agregar metodos find y findAll a ProcesoAdopcionPersistence

--- s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/ProcesoAdopcionPersistence.java
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/ProcesoAdopcionPersistence.java
@@ -6,9 +6,11 @@
 package co.edu.uniandes.csw.mascotas.persistence;
 
 import co.edu.uniandes.csw.mascotas.entities.ProcesoAdopcionEntity;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ejb.Stateless;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -24,6 +26,15 @@ public class ProcesoAdopcionPersistence {
         em.persist(proceso);
         
         return proceso;
+    }
+    
+    public ProcesoAdopcionEntity find(Long procesoID){
+        return em.find(ProcesoAdopcionEntity.class, procesoID);
+    }
+    
+    public List<ProcesoAdopcionEntity> findAll(){
+        TypedQuery query = em.createQuery("select u from ProcesoAdopcionEntity u", ProcesoAdopcionEntity.class);
+        return query.getResultList();
     }
     
 }
