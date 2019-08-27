11da76fd2ad6a5c6c85fe916310b4808ad1cda9a
lilyaita <la.duque@uniandes.edu.co>
2019-08-26 20:11:53 -0500

    MascotaPerdida probando

--- s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/MascotaPerdidaPersistence.java
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/MascotaPerdidaPersistence.java
@@ -6,7 +6,11 @@
 package co.edu.uniandes.csw.mascotas.persistence;
 
 import co.edu.uniandes.csw.mascotas.entities.MascotaPerdidaEntity;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 /**
  *
@@ -14,8 +18,34 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class MascotaPerdidaPersistence {
+    @PersistenceContext (unitName="mascotasPU")
+    protected EntityManager em;
+    /**
+     *  Persiste una mascota <br> 
+     *  @param mascotaPerdidaEntity mascota que va a persitir
+     *  @return  mascotaPerdidaEntity
+     */
     public MascotaPerdidaEntity create  (MascotaPerdidaEntity mascota)
     {
-        throw new java.lang.UnsupportedOperationException("Not supported yet.");
+        em.persist(mascota);
+        return mascota;
+    }
+    /**
+     * Se busca una mascota con el Id que pasan por parámetro.
+     * @param mascotaId El id de la mascota que se va a buscar.
+     * @return MascotaPerdidaEntity la mascota que buscaba, si no se encuenta null.
+     */
+    public MascotaPerdidaEntity find (Long mascotaId)
+    {
+        return em.find(MascotaPerdidaEntity.class, mascotaId);
+    }
+    /**
+     *  Retorna todas las mascotas perdidas que hay en la tabla.
+     *  @return List <MascotaPerdidaEntity> Lista con todas las mascotas perdidas que hay.
+     */
+    public List <MascotaPerdidaEntity> findAll()
+    {
+        TypedQuery <MascotaPerdidaEntity> query = em.createQuery("select u from MascotaPerdidaEntity u",MascotaPerdidaEntity.class);
+        return query.getResultList();
     }
 }
