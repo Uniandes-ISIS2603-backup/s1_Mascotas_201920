9916648d9b6056eb95bc0c568f3d50dfbfdbb7bd
Tobia Gasparoni <tr.gasparoni@uniandes.edu.co>
2019-08-29 00:13:12 -0500

    Todos los tests de Usuario terminados

--- s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/UsuarioPersistence.java
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/persistence/UsuarioPersistence.java
@@ -9,21 +9,72 @@ import co.edu.uniandes.csw.mascotas.entities.UsuarioEntity;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 /**
  *
- * @author Estudiante
+ * @author Tobia Gasparoni
  */
+import javax.persistence.TypedQuery;
 @Stateless
 public class UsuarioPersistence
 {
     @PersistenceContext(unitName = "mascotasPU")
     protected EntityManager em;
     
+    /**
+     * 
+     * @param usuario
+     * @return 
+     */
     public UsuarioEntity create(UsuarioEntity usuario)
     {
         em.persist(usuario);
         return usuario;
-        //throw new java.lang.UnsupportedOperationException("Not supported yet.");
     }
+    
+    /**
+     * 
+     * @param usuarioID
+     * @return 
+     */
+    public UsuarioEntity find(Long usuarioID)
+    {
+        return em.find(UsuarioEntity.class, usuarioID);
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    public List<UsuarioEntity> findAll()
+    {
+        TypedQuery query = em.createQuery("SELECT u FROM UsuarioEntity u", UsuarioEntity.class);
+        return query.getResultList();
+    }
+    
+    /**
+     * 
+     * @param usuarioID 
+     */
+    public void delete(Long usuarioID)    
+    {
+        UsuarioEntity aSerBorrado = find(usuarioID);
+        em.remove(aSerBorrado);
+    }
+    
+    /**
+     * 
+     * @param usuario
+     * @return 
+     */
+    public UsuarioEntity update(UsuarioEntity usuario)
+    {
+        UsuarioEntity nueva = em.find(UsuarioEntity.class, usuario.getId());
+        em.merge(usuario);
+
+        return find(usuario.getId());
+    }
+    
+    
 }
--- s1_mascotas-back/src/test/java/co/edu/uniandes/csw/mascotas/test/persistence/UsuarioPersistenceTest.java
+++ s1_mascotas-back/src/test/java/co/edu/uniandes/csw/mascotas/test/persistence/UsuarioPersistenceTest.java
@@ -7,6 +7,10 @@ package co.edu.uniandes.csw.mascotas.test.persistence;
 
 import co.edu.uniandes.csw.mascotas.entities.UsuarioEntity;
 import co.edu.uniandes.csw.mascotas.persistence.UsuarioPersistence;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,6 +20,7 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -23,7 +28,7 @@ import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 /**
  *
- * @author Estudiante
+ * @author Tobia Gasparoni
  */
 @RunWith(Arquillian.class)
 public class UsuarioPersistenceTest
@@ -37,11 +42,28 @@ public class UsuarioPersistenceTest
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
     
+    ArrayList<UsuarioEntity> usuarios;
+    
     @Inject
     private UsuarioPersistence usuarioPrueba;
     
     @PersistenceContext
     private EntityManager em;
+    
+    @Before
+    public void prepararDatos()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        usuarios = new ArrayList<>();
+        
+        for(int i = 0; i < (new Random()).nextInt((50-10) + 1) + 10; i++)
+        {
+            UsuarioEntity usuario = factory.manufacturePojo(UsuarioEntity.class);
+            UsuarioEntity resultado = usuarioPrueba.create(usuario);
+            Assert.assertNotNull(resultado);
+            usuarios.add(resultado);
+        }
+    }
     
     @Test
     public void testCreate()
@@ -53,5 +75,57 @@ public class UsuarioPersistenceTest
         
         UsuarioEntity entity = em.find(UsuarioEntity.class, resultado.getId());
         Assert.assertEquals(usuario.getNombre(), entity.getNombre());
+    }
+    
+    @Test
+    public void testFindAll()
+    {        
+        List<UsuarioEntity> segundaLista = (List) usuarioPrueba.findAll();
+        Iterator it = usuarios.iterator();
+        while(it.hasNext())
+        {
+            UsuarioEntity next = (UsuarioEntity) it.next();
+            Assert.assertTrue(segundaLista.contains(next));
+        }
+    }
+    
+    @Test
+    public void testFind()
+    {
+        UsuarioEntity usuarioLista = usuarios.get(0);
+        Assert.assertNotNull(usuarioLista);
+        
+        UsuarioEntity usuarioPersistencia = usuarioPrueba.find(usuarioLista.getId());
+        Assert.assertNotNull(usuarioPersistencia);
+        
+        Assert.assertEquals(usuarioPersistencia, usuarioLista);
+        
+    }
+    
+    @Test
+    public void testDelete()
+    {
+        UsuarioEntity usuarioLista = usuarios.get(0);
+        usuarios.remove(usuarioLista);
+        usuarioPrueba.delete(usuarioLista.getId());
+        UsuarioEntity usuarioQueDeberiaSerNull = usuarioPrueba.find(usuarioLista.getId());
+        Assert.assertNull(usuarioQueDeberiaSerNull);
+    }
+    
+    @Test
+    public void testUpdate()
+    {
+        UsuarioEntity usuarioLista = usuarios.get(0);
+        usuarioLista.setCorreo("tr.gasparoni@uniandes.edu.co");
+        usuarioLista.setCelular(5743868);
+        usuarioLista.setCiudad("Barranquilla");
+        
+        usuarioPrueba.update(usuarioLista);
+        
+        UsuarioEntity usuarioActualizado = usuarioPrueba.find(usuarioLista.getId());
+        Assert.assertEquals(usuarioLista.getCorreo(), usuarioActualizado.getCorreo());
+        Assert.assertEquals(usuarioLista.getCelular(), usuarioActualizado.getCelular());
+        Assert.assertEquals(usuarioLista.getCiudad(), usuarioActualizado.getCiudad());
+        
     }
 }
