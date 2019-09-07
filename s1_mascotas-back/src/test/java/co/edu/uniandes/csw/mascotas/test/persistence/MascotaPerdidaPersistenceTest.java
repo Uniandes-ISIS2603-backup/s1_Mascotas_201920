5259e014d6f298de50b208693d15865109f0c91e
lilyaita <la.duque@uniandes.edu.co>
2019-09-07 07:52:21 -0500

    Aumentar cobertura en test

--- s1_mascotas-back/src/test/java/co/edu/uniandes/csw/mascotas/test/persistence/MascotaPerdidaPersistenceTest.java
+++ s1_mascotas-back/src/test/java/co/edu/uniandes/csw/mascotas/test/persistence/MascotaPerdidaPersistenceTest.java
@@ -74,8 +74,13 @@ public class MascotaPerdidaPersistenceTest
        MascotaPerdidaEntity entity =
            em.find(MascotaPerdidaEntity.class,result.getId());
         
-       
-       Assert.assertEquals(ma.find(result.getId()),result);
+       MascotaPerdidaEntity other =  ma.find(result.getId());
+       Assert.assertEquals(other,result);
+       Assert.assertEquals(other.getDescripcion(),result.getDescripcion());
+       Assert.assertEquals(other.getEspecie(),result.getEspecie());
+       Assert.assertEquals(other.getFechaPerdida(),result.getFechaPerdida());
+       Assert.assertEquals(other.getLugar(),result.getLugar());
+       Assert.assertEquals(other.getRaza(),result.getRaza());
        
     }
     @Test
