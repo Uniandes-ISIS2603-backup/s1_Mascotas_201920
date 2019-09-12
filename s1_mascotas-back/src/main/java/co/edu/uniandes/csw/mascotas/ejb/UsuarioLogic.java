32867174dbcf686ede9209071ff491588a4ceb8a
Tobia Gasparoni <tr.gasparoni@uniandes.edu.co>
2019-09-12 08:29:09 -0500

    Se hizo la logica de usuario y las pruebas de create mas no de update

--- s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/ejb/UsuarioLogic.java
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/ejb/UsuarioLogic.java
@@ -7,6 +7,7 @@ package co.edu.uniandes.csw.mascotas.ejb;
 
 import co.edu.uniandes.csw.mascotas.persistence.UsuarioPersistence;
 import co.edu.uniandes.csw.mascotas.entities.UsuarioEntity;
+import co.edu.uniandes.csw.mascotas.exceptions.BusinessLogicException;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -19,11 +20,108 @@ public class UsuarioLogic
 {
     @Inject
     private UsuarioPersistence persistence;
-}
-
-@RunWith(Arquillian.class)
-public UsuarioEntity createUsuario(UsuarioEntity usuario) throws BusinessLogicException
-{
-    usuario = persistence.create(usuario);
-    return usuario;
+    
+    public UsuarioEntity createUsuario(UsuarioEntity usuario) throws BusinessLogicException
+    {  
+        //Nombre
+        if(usuario.getNombre() == null)
+        {
+            throw new BusinessLogicException("El nombre del usuario esta vacio");
+        }
+    
+        //Correo
+        if(usuario.getCorreo() == null)
+        {
+            throw new BusinessLogicException("El correo del usuario esta vacio");
+        }
+        
+        String[] mitadesCorreo = usuario.getCorreo().split("@");
+        if(mitadesCorreo.length != 2)
+        {
+            throw new BusinessLogicException("El correo no tiene arroba (@)");
+        }
+        
+        if(mitadesCorreo[0].equals("") || mitadesCorreo[1].equals(""))
+        {
+            throw new BusinessLogicException("El correo debe tener texto del lado izquiero y derecho de la arroba (@)");
+        }
+        
+        String[] mitadesCorreoPunto = usuario.getCorreo().split(".");
+        if(mitadesCorreo.length != 2)
+        {
+            throw new BusinessLogicException("El correo no tiene punto (.)");
+        }
+        
+        if(mitadesCorreoPunto[0].equals("") || mitadesCorreoPunto[1].equals(""))
+        {
+            throw new BusinessLogicException("El correo debe tener texto del lado izquiero y derecho del punto (.)");
+        }
+        
+        //Ciudad
+        if(usuario.getCiudad() == null)
+        {
+            throw new BusinessLogicException("La ciudad del usuario esta vacia");
+        }
+        
+        //Celular
+        if(usuario.getCelular() == null)
+        {
+            throw new BusinessLogicException("El celular del usuario esta vacio");
+        }
+        
+        usuario = persistence.create(usuario);
+        return usuario;
+    }
+    
+    public UsuarioEntity updateUsuario(UsuarioEntity usuario) throws BusinessLogicException
+    {  
+        //Nombre
+        if(usuario.getNombre() == null)
+        {
+            throw new BusinessLogicException("El nombre del usuario esta vacio");
+        }
+    
+        //Correo
+        if(usuario.getCorreo() == null)
+        {
+            throw new BusinessLogicException("El correo del usuario esta vacio");
+        }
+        
+        String[] mitadesCorreo = usuario.getCorreo().split("@");
+        if(mitadesCorreo.length != 2)
+        {
+            throw new BusinessLogicException("El correo no tiene arroba (@)");
+        }
+        
+        if(mitadesCorreo[0].equals("") || mitadesCorreo[1].equals(""))
+        {
+            throw new BusinessLogicException("El correo debe tener texto del lado izquiero y derecho de la arroba (@)");
+        }
+        
+        String[] mitadesCorreoPunto = usuario.getCorreo().split(".");
+        if(mitadesCorreo.length != 2)
+        {
+            throw new BusinessLogicException("El correo no tiene punto (.)");
+        }
+        
+        if(mitadesCorreoPunto[0].equals("") || mitadesCorreoPunto[1].equals(""))
+        {
+            throw new BusinessLogicException("El correo debe tener texto del lado izquiero y derecho del punto (.)");
+        }
+        
+        //Ciudad
+        if(usuario.getCiudad() == null)
+        {
+            throw new BusinessLogicException("La ciudad del usuario esta vacia");
+        }
+        
+        //Celular
+        if(usuario.getCelular() == null)
+        {
+            throw new BusinessLogicException("El celular del usuario esta vacio");
+        }
+        
+        usuario = persistence.update(usuario);
+        return usuario;
+    }
 }
--- s1_mascotas-back/src/test/java/co/edu/uniandes/csw/mascotas/test/logic/UsuarioLogicTest.java
+++ s1_mascotas-back/src/test/java/co/edu/uniandes/csw/mascotas/test/logic/UsuarioLogicTest.java
@@ -5,10 +5,121 @@
  */
 package co.edu.uniandes.csw.mascotas.test.logic;
 
+import co.edu.uniandes.csw.mascotas.ejb.UsuarioLogic;
+import co.edu.uniandes.csw.mascotas.entities.UsuarioEntity;
+import co.edu.uniandes.csw.mascotas.exceptions.BusinessLogicException;
+import co.edu.uniandes.csw.mascotas.persistence.UsuarioPersistence;
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author Tobia Gasparoni
  */
+@RunWith(Arquillian.class)
 public class UsuarioLogicTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(UsuarioEntity.class.getPackage())
+                .addPackage(UsuarioLogic.class.getPackage())
+                .addPackage(UsuarioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    private PodamFactory factory = new PodamFactoryImpl();
+    
+    @Inject
+    private UsuarioLogic usuarioLogic;
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioNombreNull() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setNombre(null);
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCiudadNull() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCiudad(null);
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoNull() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo(null);
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoSinArroba() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo("tobia1999gmail.com");
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoParteIzquierdaVacia() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo("@gmail.com");
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoParteDerechaVacia() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo("tobia1999@");
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoSinPunto() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo("tobia1999@gmailcom");
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoPuntoIzquierdaVacia() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo("tobia1999@.com");
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCorreoPuntoDerechaVacia() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCorreo("tobia1999@gmail.");
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    @Test (expected = BusinessLogicException.class)
+    public void createUsuarioCelularNull() throws BusinessLogicException
+    {
+        UsuarioEntity newUsuario = factory.manufacturePojo(UsuarioEntity.class);
+        newUsuario.setCelular(null);
+        usuarioLogic.createUsuario(newUsuario);
+    }
+    
+    
     
 }
