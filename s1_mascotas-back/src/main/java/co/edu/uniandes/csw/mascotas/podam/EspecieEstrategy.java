0008323c9a3a679aac53605abfb83eb6a03a87d7
lilyaita <la.duque@uniandes.edu.co>
2019-11-11 10:16:52 -0500

    intento correccion del bug2

--- s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/podam/EspecieEstrategy.java
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/podam/EspecieEstrategy.java
@@ -9,13 +9,23 @@ package co.edu.uniandes.csw.mascotas.podam;
  * Estrategy de especie, solo puede tomar valores entre 0 y 1. Debido a que solo existen dos enums de especie.
  * @author Lily 
  */
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import uk.co.jemos.podam.common.AttributeStrategy;
 
 public class EspecieEstrategy implements AttributeStrategy<Integer> {
     @Override
     public Integer getValue() {
-        Random r = new Random();
-        return r.nextInt(1);
+        int y=0;
+        try {
+            Random r = SecureRandom.getInstanceStrong();
+            y= r.nextInt(1);
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(EspecieEstrategy.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return y;
     }
 }
