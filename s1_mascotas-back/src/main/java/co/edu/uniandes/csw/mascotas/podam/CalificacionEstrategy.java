fe2bcc869ab233886f054bf42019318a7dc005ce
lilyaita <la.duque@uniandes.edu.co>
2019-11-11 10:09:59 -0500

    Intento de correccion del bug

--- s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/podam/CalificacionEstrategy.java
+++ s1_mascotas-back/src/main/java/co/edu/uniandes/csw/mascotas/podam/CalificacionEstrategy.java
@@ -10,18 +10,30 @@ package co.edu.uniandes.csw.mascotas.podam;
  * @author Lily Duque
  */
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import uk.co.jemos.podam.common.AttributeStrategy;
 
 public class CalificacionEstrategy implements AttributeStrategy<Integer> {
+     
     @Override
     public Integer getValue() {
-        Random r = new Random();
-        int y=r.nextInt(5);
-        while (0>=y || 5<y)
-        {
+        Random r;
+        int y=1;
+        try {
+            r = SecureRandom.getInstanceStrong();
             y=r.nextInt(5);
+            while (0>=y || 5<y)
+            {
+                y=r.nextInt(5);
+            }
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(CalificacionEstrategy.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
         return y;
     }
 }
