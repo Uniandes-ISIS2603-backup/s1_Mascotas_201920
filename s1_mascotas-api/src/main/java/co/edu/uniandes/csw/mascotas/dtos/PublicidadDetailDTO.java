1e222caee82a4a88a3ee6a954e422bd2197452ab
gdrozo <gd,rozo@uniandes.edu.co>
2019-11-13 22:10:41 -0500

    Creacion PublicidadDetail

---
+++ s1_mascotas-api/src/main/java/co/edu/uniandes/csw/mascotas/dtos/PublicidadDetailDTO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.mascotas.dtos;
+
+import co.edu.uniandes.csw.mascotas.entities.MultimediaEntity;
+import co.edu.uniandes.csw.mascotas.entities.PublicidadEntity;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author German Rozo
+ */
+
+public class PublicidadDetailDTO extends PublicidadDTO implements Serializable {
+
+    private List<MultimediaDTO> multimedia;
+
+    public PublicidadDetailDTO() {}
+
+    public PublicidadDetailDTO(PublicidadEntity publicidad) 
+    {
+        super(publicidad);
+        if (publicidad != null) 
+        {
+            multimedia = new ArrayList<>();
+            
+            for (MultimediaEntity multimediaEntity : publicidad.getMultimedia()) 
+            {
+                multimedia.add(new MultimediaDTO(multimediaEntity));
+            }
+        }
+    }
+
+    public PublicidadEntity toEntity()
+    {
+        PublicidadEntity entity = super.toEntity();
+        
+        List<MultimediaEntity> list= new ArrayList<>();
+        
+        for (MultimediaDTO multimediaDTO : multimedia) {
+            list.add(multimediaDTO.toEntity());
+        }
+        
+        entity.setMultimedia(list);
+        return entity;
+    }
+
+    public List<MultimediaDTO> getMultimedia() {
+        return multimedia;
+    }
+
+    public void setMultimedia(List<MultimediaDTO> multimedia) {
+        this.multimedia = multimedia;
+    }
+  
+}
+
